2871f4ee20e123e400d608cf0e62b890311154d1
PraezeCodes <108436639+PraezeCodes@users.noreply.github.com>
2022-09-27 12:44:38 +0100

    Update server.js

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ next();
 myapp.get('/', function(req, res) {
     res.send("<h1>Hello World!</h1>");
 });
-
+myapp.use(express.static(__dirname + '/commerce'));
 
 
 const portr = process.env.PORT || 3000;
