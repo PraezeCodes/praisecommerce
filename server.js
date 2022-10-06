fb8cc1a1786e585e3cfbcb54d58376589018ea40
PraezeCodes <108436639+PraezeCodes@users.noreply.github.com>
2022-10-06 12:55:48 +0100

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var bodyParser = require('body-parser');
 const jwt = require('jsonwebtoken');
 const client = new Client({
   connectionString: "postgres://qdyuodaxrolgjb:40d0c9eceda93438fbe662067436e0e5d1d5e3e6f95c5099844b38a8e645bca9@ec2-52-70-86-157.compute-1.amazonaws.com:5432/de3s7pn71jrsv",
-  ssl: true,
+  ssl: {rejectUnauthorized: false},
 }) 
 
 client.connect();
@@ -60,9 +60,9 @@ arr['reg_date'] = reg_date;
 
 datae['data'] = arr;
 datae['message'] = "Your registration was successful";
- res.send(datae);
 }
 });
+ res.send(datae);
 });
 
 
